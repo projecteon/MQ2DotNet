3449bf54f537e8196e22115751391f275c72cc8f
Tim <tconnell@gmail.com>
2018-06-29 04:10:53 +0000

    Add xmldoc for CurrentZone members

--- MQ2DotNet/MQ2API/DataTypes/CurrentZoneType.cs
+++ MQ2DotNet/MQ2API/DataTypes/CurrentZoneType.cs
@@ -1,21 +1,76 @@
-﻿namespace MQ2DotNet.MQ2API.DataTypes
+namespace MQ2DotNet.MQ2API.DataTypes
 {
     // TODO: Check and handle indexed members for CurrentZoneType
     public class CurrentZoneType : MQ2DataType
     {
+        /// <summary>
+        /// Memory address of the TODO: struct
+        /// </summary>
         public IntType Address => GetMember<IntType>("Address");
+        
+        /// <summary>
+        /// ID of the zone
+        /// </summary>
         public IntType ID => GetMember<IntType>("ID");
+
+        /// <summary>
+        /// Full name of the zone e.g. "The Plane of Knowledge"
+        /// </summary>
         public StringType Name => GetMember<StringType>("Name");
+        
+        /// <summary>
+        /// Short name of the zone e.g. "PoKnowledge"
+        /// </summary>
         public StringType ShortName => GetMember<StringType>("ShortName");
+        
+        /// <summary>
+        /// TODO: Description
+        /// </summary>
         public IntType Type => GetMember<IntType>("Type");
+        
+        /// <summary>
+        /// Value of gravity in the zone
+        /// </summary>
         public FloatType Gravity => GetMember<FloatType>("Gravity");
+        
+        /// <summary>
+        /// Type of sky in the zone
+        /// </summary>
         public IntType SkyType => GetMember<IntType>("SkyType");
+        
+        /// <summary>
+        /// Minimum setting for far clip plane
+        /// </summary>
         public FloatType MinClip => GetMember<FloatType>("MinClip");
+        
+        /// <summary>
+        /// Maximum setting for far clip plane
+        /// </summary>
         public FloatType MaxClip => GetMember<FloatType>("MaxClip");
+        
+        /// <summary>
+        /// TODO: Description
+        /// </summary>
         public IntType ZoneType => GetMember<IntType>("ZoneType");
+        
+        /// <summary>
+        /// Is the zone a dungeon, i.e. mounts cannot be used
+        /// </summary>
         public BoolType Dungeon => Indoor;
+        
+        /// <summary>
+        /// Is the zone indoors, i.e. mounts cannot be used
+        /// </summary>
         public BoolType Indoor => GetMember<BoolType>("Indoor");
+        
+        /// <summary>
+        /// Is the zone outdoors, i.e. mounts can be used
+        /// </summary>
         public BoolType Outdoor => GetMember<BoolType>("Outdoor");
+        
+        /// <summary>
+        /// Binding in the zone is disabled TODO: For melee only or everyone?
+        /// </summary>
         public BoolType NoBind => GetMember<BoolType>("NoBind");
     }
 }
