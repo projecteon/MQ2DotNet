1067e3a6f2633a96c9f26419461a3e2cf6e4b9f3
Alynel <Alynel@home>
2019-08-05 03:44:35 +0200

    Fix potential crash on assembly load

--- MQ2DotNet/MQ2API/MQ2TypeFactory.cs
+++ MQ2DotNet/MQ2API/MQ2TypeFactory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
@@ -54,41 +55,54 @@ namespace MQ2DotNet.MQ2API
                 _registeredAssemblies.Add(assembly);
             }
 
-            // Can ignore any assemblies that don't reference this one
-            var thisAssembly = Assembly.GetExecutingAssembly();
-            if (assembly != thisAssembly && !assembly.GetReferencedAssemblies().Contains(thisAssembly.GetName()))
-                return;
-
-            // Find all subclasses of MQ2DataType, and get their MQ2Type attribute
-            foreach (var type in assembly.GetTypes().Where(t => t.IsSubclassOf(typeof(MQ2DataType))))
+            try
             {
-                var mq2Type = type.GetCustomAttribute<MQ2TypeAttribute>();
-                // Not sure why you'd want it without the attribute but I'll allow it
-                if (mq2Type == null)
-                    continue;
+                // Can ignore any assemblies that don't reference this one
+                var thisAssembly = Assembly.GetExecutingAssembly();
+                if (assembly != thisAssembly && !assembly.GetReferencedAssemblies().Select(a => a.Name).Contains(thisAssembly.GetName().Name))
+                    return;
 
-                // It needs a public constructor with a single MQ2TypeVar as a parameter
-                var ctor = type.GetConstructor(new[] {typeof(MQ2TypeVar)});
+                // Find all subclasses of MQ2DataType, and get their MQ2Type attribute
+                foreach (var type in assembly.GetTypes())
+                {
+                    if (!type.IsSubclassOf(typeof(MQ2DataType)))
+                        continue;
 
-                // If the type is defined in this assembly, allow an internal constructor too
-                if (ctor == null && assembly == Assembly.GetExecutingAssembly())
-                    ctor = type.GetConstructors(BindingFlags.NonPublic | BindingFlags.Instance).SingleOrDefault(c =>
-                    {
-                        var parameters = c.GetParameters();
-                        return c.IsAssembly && parameters.Length == 2 
-                                            && parameters[0].ParameterType == typeof(MQ2TypeFactory) 
-                                            && parameters[1].ParameterType == typeof(MQ2TypeVar);
-                    });
+                    var mq2Type = type.GetCustomAttribute<MQ2TypeAttribute>();
+                    // Not sure why you'd want it without the attribute but I'll allow it
+                    if (mq2Type == null)
+                        continue;
 
-                if (ctor == null)
-                    throw new MissingMethodException($"{type.Name} is marked as an MQ2Type, but does not have a constructor taking an MQ2TypeFactory & MQ2TypeVar");
-                
-                // Create a lambda that creates an instance of this type
-                // Ivan says it's fast: https://ru.stackoverflow.com/questions/860901/%D0%A1%D1%83%D1%89%D0%B5%D1%81%D1%82%D0%B2%D1%83%D0%B5%D1%82-%D0%BB%D0%B8-%D0%B2%D0%BE%D0%B7%D0%BC%D0%BE%D0%B6%D0%BD%D0%BE%D1%81%D1%82%D1%8C-%D1%81%D0%BE%D0%B7%D0%B4%D0%B0%D0%B2%D0%B0%D1%82%D1%8C-%D0%BE%D0%B1%D1%8A%D0%B5%D0%BA%D1%82-%D0%BE%D0%BF%D1%80%D0%B5%D0%B4%D0%B5%D0%BB%D0%B5%D0%BD%D0%BD%D0%BE%D0%B3%D0%BE-%D1%82%D0%B8%D0%BF%D0%B0-%D0%B1%D0%B5%D0%B7-%D0%B8%D1%81%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F/860921#860921
-                var typeFactoryParam = Expression.Parameter(typeof(MQ2TypeFactory));
-                var typeVarParam = Expression.Parameter(typeof(MQ2TypeVar));
-                var constructor = Expression.Lambda<Func<MQ2TypeFactory, MQ2TypeVar, MQ2DataType>>(Expression.New(ctor, typeFactoryParam, typeVarParam), typeFactoryParam, typeVarParam);
-                Register(mq2Type.TypeName, constructor.Compile());
+                    // It needs a public constructor with a single MQ2TypeVar as a parameter
+                    var ctor = type.GetConstructor(new[] {typeof(MQ2TypeFactory), typeof(MQ2TypeVar)});
+
+                    // If the type is defined in this assembly, allow an internal constructor too
+                    if (ctor == null && assembly == Assembly.GetExecutingAssembly())
+                        ctor = type.GetConstructors(BindingFlags.NonPublic | BindingFlags.Instance).SingleOrDefault(c =>
+                        {
+                            var parameters = c.GetParameters();
+                            return c.IsAssembly && parameters.Length == 2
+                                                && parameters[0].ParameterType == typeof(MQ2TypeFactory)
+                                                && parameters[1].ParameterType == typeof(MQ2TypeVar);
+                        });
+
+                    if (ctor == null)
+                        throw new MissingMethodException(
+                            $"{type.Name} is marked as an MQ2Type, but does not have a constructor taking an MQ2TypeFactory & MQ2TypeVar");
+
+                    // Create a lambda that creates an instance of this type
+                    // Ivan says it's fast: https://ru.stackoverflow.com/questions/860901/%D0%A1%D1%83%D1%89%D0%B5%D1%81%D1%82%D0%B2%D1%83%D0%B5%D1%82-%D0%BB%D0%B8-%D0%B2%D0%BE%D0%B7%D0%BC%D0%BE%D0%B6%D0%BD%D0%BE%D1%81%D1%82%D1%8C-%D1%81%D0%BE%D0%B7%D0%B4%D0%B0%D0%B2%D0%B0%D1%82%D1%8C-%D0%BE%D0%B1%D1%8A%D0%B5%D0%BA%D1%82-%D0%BE%D0%BF%D1%80%D0%B5%D0%B4%D0%B5%D0%BB%D0%B5%D0%BD%D0%BD%D0%BE%D0%B3%D0%BE-%D1%82%D0%B8%D0%BF%D0%B0-%D0%B1%D0%B5%D0%B7-%D0%B8%D1%81%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F/860921#860921
+                    var typeFactoryParam = Expression.Parameter(typeof(MQ2TypeFactory));
+                    var typeVarParam = Expression.Parameter(typeof(MQ2TypeVar));
+                    var constructor = Expression.Lambda<Func<MQ2TypeFactory, MQ2TypeVar, MQ2DataType>>(
+                        Expression.New(ctor, typeFactoryParam, typeVarParam), typeFactoryParam, typeVarParam);
+                    Register(mq2Type.TypeName, constructor.Compile());
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Error finding types in assembly: " + assembly.GetName());
+                Debug.WriteLine(e);
             }
         }
 
